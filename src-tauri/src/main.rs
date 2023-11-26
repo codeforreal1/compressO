aaeadcf37fbc0c2ab9da095e667db79637cf1b83
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-11-26 22:08:43 +0545

    format

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 
-use tauri::api::path;
-
 use libs::ffmpeg;
-use libs::filesystem;
 
 #[tauri::command]
 async fn compress(path: &str) -> Result<String, String> {
-    println!("File received");
+    println!("[info] File received");
     return match ffmpeg::compress(path).await {
         Ok(result) => Ok(result),
         Err(err) => Err(err),
@@ -17,8 +14,6 @@ async fn compress(path: &str) -> Result<String, String> {
 }
 
 fn main() {
-    println!("{:?}", path::cache_dir());
-    filesystem::create_cache_dir().unwrap();
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![compress])
         .run(tauri::generate_context!())
