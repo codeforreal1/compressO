2925e69c8d9cceb15e6597005668dc3fcf645f6d
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-05-13 23:35:22 +0545

    windows executable

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,7 @@ const withBundleAnalyzer = analyzeBundle({
 const nextConfig = withBundleAnalyzer({
   output: "export",
   distDir: "./dist",
+  cleanDistDir:true,
   webpack(config, { webpack }) {
     config.module.rules.push({
       test: /\.svg$/,
