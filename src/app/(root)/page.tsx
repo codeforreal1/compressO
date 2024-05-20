93b04dc9ff9669b5a1fb9e1e586293aca2689636
codeforreal1 <codeforreal@outlook.com>
2024-05-20 09:19:05 +0545

    Adjust minimum window height and improve responsiveness

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -47,6 +47,7 @@ import { convertDurationToMilliseconds } from '@/utils/string'
 import DotPattern from '@/ui/Patterns/DotPattern'
 import Drawer from '@/components/Drawer'
 import Checkbox from '@/components/Checkbox'
+import { cn } from '@/utils/tailwind'
 import Setting from './Setting'
 
 type Video = {
@@ -348,6 +349,20 @@ function Root() {
     }
   }
 
+  const renderCompressButton = (
+    size: 'md' | 'lg' = 'lg',
+    className?: string,
+  ) => (
+    <Button
+      size={size}
+      color="primary"
+      onClick={handleCompression}
+      className={cn(['my-2', className ?? ''])}
+    >
+      Compress <Icon name="logo" size={size === 'md' ? 20 : 25} />
+    </Button>
+  )
+
   return (
     <section className="w-full h-full relative">
       {!video?.isFileSelected ? <DotPattern className="opacity-40" /> : null}
@@ -379,7 +394,7 @@ function Root() {
             <div className="flex flex-col justify-center items-center">
               {video?.fileName && !video?.isCompressing ? (
                 <div className="flex justify-center items-center mb-2 gap-1">
-                  <Code className="ml-auto mr-auto text-center rounded-lg">
+                  <Code className="ml-auto mr-auto text-xs hlg:text-sm text-center rounded-lg">
                     {fileNameDisplay?.length > 50
                       ? `${fileNameDisplay?.slice(0, 20)}...${fileNameDisplay?.slice(-10)}`
                       : fileNameDisplay}
@@ -471,26 +486,28 @@ function Root() {
               {!video?.isCompressing ? (
                 video?.isCompressionSuccessful ? (
                   <section className="animate-appearance-in">
-                    <div className="flex justify-center items-center mt-6">
-                      <p className="text-4xl font-bold mx-4">{video?.size}</p>
+                    <div className="flex justify-center items-center mt-3 hlg:mt-6">
+                      <p className="text-2xl hlg:text-4xl font-bold mx-4">
+                        {video?.size}
+                      </p>
                       <Icon
                         name="curvedArrow"
                         className="text-black dark:text-white rotate-[-65deg] translate-y-[-8px]"
                         size={100}
                       />
-                      <p className="text-4xl font-bold mx-4 text-primary">
+                      <p className="text-3xl hlg:text-4xl font-bold mx-4 text-primary">
                         {video?.compressedVideo?.size}
                       </p>
                     </div>
                     {!(sizeDiff <= 0) ? (
-                      <p className="block text-7xl text-center text-green-500 mt-5">
+                      <p className="block text-5xl hlg:text-7xl text-center text-green-500 hlg:mt-5">
                         {sizeDiff.toFixed(2)?.endsWith('.00')
                           ? sizeDiff.toFixed(2)?.slice(0, -3)
                           : sizeDiff.toFixed(2)}
                         %<span className="text-large block">smaller</span>
                       </p>
                     ) : null}
-                    <div className="flex justify-center mt-10">
+                    <div className="flex justify-center mt-5 hlg:mt-10">
                       <Button
                         className="flex justify-center items-center"
                         color="success"
@@ -530,25 +547,27 @@ function Root() {
                   </section>
                 ) : (
                   <>
-                    <section className="my-6 flex items-center space-x-4 justify-center gap-4">
+                    <section className="my-4 hlg:my-6 flex items-center space-x-4 justify-center gap-4">
                       <div>
                         <p className="italic text-sm text-gray-600 dark:text-gray-400">
                           Size
                         </p>
-                        <h1 className="text-4xl font-black">{video?.size}</h1>
+                        <h1 className="text-2xl hlg:text-4xl font-black">
+                          {video?.size}
+                        </h1>
                       </div>
                       <Divider orientation="vertical" className="h-10" />
                       <div>
                         <p className="italic text-sm text-gray-600 dark:text-gray-400">
                           Extension
                         </p>
-                        <h1 className="text-4xl font-black">
+                        <h1 className="text-2xl hlg:text-4xl font-black">
                           {video?.extension ?? '-'}
                         </h1>
                       </div>
                     </section>
                     <Divider />
-                    <section className="my-4">
+                    <section className="my-2 hlg:my-4">
                       <div className="flex items-center my-2">
                         <Checkbox
                           isSelected={shouldMuteVideo}
@@ -668,14 +687,8 @@ function Root() {
                         </Select>
                       </div>
                     </section>
-                    <Button
-                      size="lg"
-                      color="primary"
-                      onClick={handleCompression}
-                      className="mt-2"
-                    >
-                      Compress <Icon name="logo" size={25} />
-                    </Button>
+                    {renderCompressButton('md', 'hlg:hidden')}
+                    {renderCompressButton('lg', 'hidden hlg:flex hlg:mt-2')}
                   </>
                 )
               ) : null}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,61 +1,71 @@
-import type { Config } from "tailwindcss";
-import { nextui } from "@nextui-org/theme";
+import type { Config } from 'tailwindcss'
+import { nextui } from '@nextui-org/theme'
 
 const WIDTHS = Object.freeze({
-  xs: "320px",
-  sm: "576px",
-  md: "768px",
-  lg: "992px",
-  xl: "1280px",
-  "2xl": "1440px",
-  "3xl": "1660px",
-});
+  xs: '320px',
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1280px',
+  '2xl': '1440px',
+  '3xl': '1660px',
+})
+
+const CUSTOM_MQ = {
+  hxs: { raw: `(min-height: ${WIDTHS.xs})` },
+  hsm: { raw: `(min-height: ${WIDTHS.sm})` },
+  hmd: { raw: `(min-height: ${WIDTHS.md})` },
+  hlg: { raw: `(min-height: ${WIDTHS.lg})` },
+  hxl: { raw: `(min-height: ${WIDTHS.xl})` },
+  h2xl: { raw: `(min-height: ${WIDTHS['2xl']})` },
+  h3xl: { raw: `(min-height: ${WIDTHS['3xl']})` },
+}
 
 const config: Config = {
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [
     nextui({
       addCommonColors: true,
     }),
   ],
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
     // NextUI Components
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        primary: "#7f46e2",
-        secondary: "#ff6ae7",
-        black1: "#000000",
-        gray1: "#c2c2c2",
-        white1: "#ffffff",
+        primary: '#7f46e2',
+        secondary: '#ff6ae7',
+        black1: '#000000',
+        gray1: '#c2c2c2',
+        white1: '#ffffff',
       },
       fontFamily: {
-        poppins: ["var(--font-poppins)"],
+        poppins: ['var(--font-poppins)'],
       },
-      screens: WIDTHS,
+      screens: { ...WIDTHS, ...CUSTOM_MQ },
       maxWidth: WIDTHS,
       animation: {
-        "meteor-effect": "meteor 5s linear infinite",
+        'meteor-effect': 'meteor 5s linear infinite',
       },
       keyframes: {
         meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
-          "70%": { opacity: "1" },
-          "100%": {
-            transform: "rotate(215deg) translateX(-500px)",
-            opacity: "0",
+          '0%': { transform: 'rotate(215deg) translateX(0)', opacity: '1' },
+          '70%': { opacity: '1' },
+          '100%': {
+            transform: 'rotate(215deg) translateX(-500px)',
+            opacity: '0',
           },
         },
       },
     },
   },
-};
-export default config;
+}
+export default config
