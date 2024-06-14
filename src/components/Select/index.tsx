b28e98cee51f917ae09c395a5fcbf6b1e7db7472
codeforreal1 <codeforreal@outlook.com>
2024-06-14 17:50:16 +0545

    Apply consistent styles to Select and Tooltip as well

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -5,10 +5,27 @@ import {
   type SelectProps as NextUISelectProps,
   type SelectItemProps as NextUISelectItemProps,
 } from '@nextui-org/select'
+import { blurCSS } from '@/ui/BackdropBlur'
+import { cn } from '@/utils/tailwind'
+import { getPlatform } from '@/utils/fs'
+
+const { isWindows, isMacOS } = getPlatform()
 
 interface SelectProps extends NextUISelectProps {}
 function Select(props: SelectProps) {
-  return <NextUISelect radius="lg" {...props} />
+  return (
+    <NextUISelect
+      radius="lg"
+      {...props}
+      classNames={{
+        popoverContent: cn([
+          isWindows || !isMacOS ? blurCSS : '',
+          props?.classNames?.popoverContent,
+        ]),
+        ...(props?.classNames ?? {}),
+      }}
+    />
+  )
 }
 
 interface SelectItemProps extends NextUISelectItemProps {}
--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -3,12 +3,22 @@ import {
   Tooltip as NextUITooltip,
   type TooltipProps as NextUITooltipProps,
 } from '@nextui-org/tooltip'
+import { blurCSS } from '@/ui/BackdropBlur'
+import { cn } from '@/utils/tailwind'
+import { getPlatform } from '@/utils/fs'
+
+const { isWindows, isMacOS } = getPlatform()
 
 interface TooltipProps extends NextUITooltipProps {}
-
 function Tooltip(props: TooltipProps) {
   return (
-    <NextUITooltip delay={1000} showArrow size="sm" {...props}>
+    <NextUITooltip
+      delay={1000}
+      showArrow
+      size="sm"
+      {...props}
+      className={cn(isMacOS || isWindows ? blurCSS : '', props?.className)}
+    >
       <span>{props?.children}</span>
     </NextUITooltip>
   )
