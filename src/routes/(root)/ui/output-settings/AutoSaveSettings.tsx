5bf45321080d8eb62b5421e3648d06648483beef
Rajib Ahmed <rajibdpi@gmail.com>
2026-08-14 03:21:07 +0600

    layout of AutoSaveSettings component for improved styling and spacing

--- src/routes/(root)/ui/output-settings/AutoSaveSettings.tsx
+++ src/routes/(root)/ui/output-settings/AutoSaveSettings.tsx
@@ -57,7 +57,7 @@ function AutoSaveSettings() {
     : 'Downloads (default)'
 
   return (
-    <div className="mb-4">
+    <div className="py-2 flex flex-col gap-2 w-full">
       <div className="flex items-center">
         <Switch
           isSelected={autoSaveEnabled}
