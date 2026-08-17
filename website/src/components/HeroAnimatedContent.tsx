a0a44103e9b04b27e6756c04350e5977ea3f3118
niraj-khatiwada <nirazzz.nk@gmail.com>
2026-08-17 16:34:47 +0545

    update website

--- website/src/components/HeroAnimatedContent.tsx
+++ website/src/components/HeroAnimatedContent.tsx
@@ -1,9 +1,8 @@
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
-import { TextBlurIn } from './effects/TextBlurIn'
-import { TextFade } from './effects/TextFade'
-import TextSlot from './effects/TextSlot'
-import { ZoomInBounce } from './effects/ZoomInBounce'
+import { TextFade } from "./effects/TextFade";
+import TextSlot from "./effects/TextSlot";
+import { ZoomInBounce } from "./effects/ZoomInBounce";
 
 function HeroAnimatedContent() {
   return (
@@ -12,46 +11,47 @@ function HeroAnimatedContent() {
         <img
           src="/app-icon.png"
           alt="Compresso Logo"
-          className="w-30 h-auto block  mx-auto!"
+          className="w-24 h-auto block mx-auto!"
         />
       </ZoomInBounce>
-      <TextBlurIn className="badge">✨ 100% Free & Open Source</TextBlurIn>
-      <h1 style={{ visibility: 'hidden', fontSize: '0' }}>
+      <h1 style={{ visibility: "hidden", fontSize: "0" }}>
         Compress any video/image
       </h1>
-      <h1>
-        <motion.span layoutId="item">Compress any </motion.span>
+      <h1 className="md:pl-37.5!">
+        <span>Compress any </span>
         <TextSlot
           texts={[
-            'mp4',
-            'png',
-            'mov',
-            'jpg',
-            'avi',
-            'jpeg',
-            'mkv',
-            'webp',
-            'webm',
-            'gif',
-            'svg',
+            "video",
+            "mp4",
+            "png",
+            "mov",
+            "jpg",
+            "avi",
+            "jpeg",
+            "mkv",
+            "webp",
+            "webm",
+            "gif",
+            "svg",
+            "image",
           ]}
-          mainClassName="inline-block md:min-w-[250px] mx-auto text-center flex justify-center"
-          staggerFrom={'first'}
-          initial={{ y: '100%' }}
+          mainClassName="inline-block md:min-w-[320px] mx-auto text-center flex justify-center"
+          staggerFrom={"first"}
+          initial={{ y: "100%" }}
           animate={{ y: 0 }}
-          exit={{ y: '-120%' }}
+          exit={{ y: "-120%" }}
           staggerDuration={0.025}
           splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-          transition={{ type: 'spring', damping: 30, stiffness: 400 }}
+          transition={{ type: "spring", damping: 30, stiffness: 400 }}
           rotationInterval={2000}
         />
         <br />
       </h1>
       <TextFade direction="down" className="subtitle">
-        <p className="gradient-text title mb-5!">Completely private</p>
+        <p className="title mb-5! scale-[0.9]">Completely private</p>
       </TextFade>
     </>
-  )
+  );
 }
 
-export default HeroAnimatedContent
+export default HeroAnimatedContent;
